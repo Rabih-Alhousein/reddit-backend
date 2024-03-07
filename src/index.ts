5134f48717649e783b344dffa23aaf7f62d9f204
rabih-alhousein <rabih.alhousein@gmail.com>
2024-03-07 14:52:28 +0200

    update domain name

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,8 @@ const main = async () => {
     disableTouch: true,
   });
 
+  app.set("trust proxy", 1);
+
   // Initialize sesssion storage.
   app.use(
     session({
@@ -73,7 +75,7 @@ const main = async () => {
         httpOnly: true, // cookie is only accessible by the web server (not by javascript)
         secure: __prod__, // cookie is only sent to the server with an encrypted request over the HTTPS protocol
         sameSite: "none", // cookie is not sent on cross-site requests (see https://owasp.org/www-community/SameSite)
-        domain: __prod__ ? "reddithub.vercel.app/login" : undefined,
+        domain: __prod__ ? "reddithub.vercel.app" : undefined,
         // for localhost, set sameSite: "lax" and secure: false
         // for sandbox testing, set sameSite: "none" and secure: true
         // for production, set sameSite: "none" and secure: true
@@ -98,7 +100,6 @@ const main = async () => {
   };
 
   // solution link: https://stackoverflow.com/questions/69333408/express-session-does-not-set-cookie
-  app.set("trust proxy", 1);
 
   await apolloServer.start();
   apolloServer.applyMiddleware({ app, cors: corsOptions });
